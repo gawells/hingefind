1d95a4fa751963d4ad30261dad07aedcc6219b59
Gordon Wells <gordon.wells@gmail.com>
2014-08-29 15:14:57 +0200

    Initial commit of transcription
    All in python except rendering. Finds the first domain correctly but
    still not functional

--- hingefind.py
+++ hingefind.py
@@ -16,12 +16,19 @@ class HingeFind:
         self.initrad = initrad
         self.maxcounter = maxcounter
         self.ndomains = ndomains
+        self.domain_count = 0
         self.cutdom = cutdom
 
-        self.mobFullCA = self.mobile.select("name CA and protein")
-        self.refFullCA = self.reference.select("name CA and protein")
-        self.mobCA = self.mobile.select("name CA and protein")
-        self.refCA = self.reference.select("name CA and protein")
+        self.mobFullCA = self.mobile.select("name CA and protein ")
+        self.refFullCA = self.reference.select("name CA and protein ")
+        self.mobCA = self.mobile.select("name CA and protein ")
+        self.refCA = self.reference.select("name CA and protein ")
+
+        self.mobDomList = {}
+        self.refDomList = {}
+
+        self.dindex = []
+        self.sup_rmsd = []
 
         try :     
             self.refCA.numAtoms() == self.mobCA.numAtoms()
@@ -36,6 +43,12 @@ class HingeFind:
     def lsubstract(self,list1,list2):
         return [l for l in list1 if l not in list2]
 
+    def arraystr(self,a):
+    	return " ".join([str(x) for x in a])
+
+    def indexSel(self,structure,indices):
+        return structure.select("index "+self.arraystr(indices))
+
     def criterion (self,mob_sel,ref_sel,eps):
     # def criterion (self,mobile,reference,eps):
         mob_coord = mob_sel.getCoords()
@@ -49,27 +62,27 @@ class HingeFind:
 
         i = 0
 
-        for (c1,c2) in [(c1,c2) for c1 in mob_coord for c2 in ref_coord]:
-            if pd.calcDistance(c1,c2) < eps:
+        while i < len(mob_coord):
+        # for (c1,c2) in [(c1,c2) for c1 in mob_coord for c2 in ref_coord]:            
+            if pd.calcDistance(mob_coord[i],ref_coord[i]) < eps:
                 mob_list.append(mob_ids[i])
                 ref_list.append(ref_ids[i])
-                i += 1
+            i += 1  
         
-        return [mob_list,ref_list]
+        mob_surv = self.indexSel(mob_sel,mob_list)
+        ref_surv = self.indexSel(ref_sel,ref_list)
 
-    def arraystr(a):
-    	return " ".join([str(x) for x in a])
+        return [mob_surv,ref_surv]
+        # return [mob_list,ref_list]
 
-    def superimpose(self,mob_sel,ref_sel,eps):
-    	survivor = criterion(mob_sel,ref_sel,eps)
+    def superimpose(self,eps):
+    	survivor = self.criterion(self.mobCA,self.refCA,eps)
     	mob_subset = survivor[0]
     	ref_subset = survivor[1]
-    	if len(mob_subset) < 5:
+    	if mob_subset.numAtoms() < 5:
     		return survivor
 
-    	mob_subset_sel = self.mobile.select("index "+arraystr(mob_subset))
-    	ref_subset_sel = self.reference.select("index "+arraystr(ref_subset))
-    	t = pd.calcTransformation(mob_subset_sel,ref_subset_sel)
+    	t = pd.calcTransformation(mob_subset,ref_subset)
     	pd.applyTransformation(t,self.mobile)
     	return survivor    	
 
@@ -92,18 +105,134 @@ class HingeFind:
     			mob_list.append(mob_id)
     			ref_list.append(ref_id)
     		i +=1
-    	return [mob_list,ref_list]
+
+        mob_subset = self.indexSel(self.mobile, mob_list)
+        ref_subset = self.indexSel(self.reference, ref_list)
+    	return [mob_subset,ref_subset]
 
     def convergence(self,eps,radius):
-    	startids = seed(self.mobCA,self.refCA,radius)
-    	startids_mob = startids[0]
-    	startids_ref = startids[1]
-    	if len(startids_mob) < 5:
-    		return startids
+    	startsel = self.seed(self.mobCA,self.refCA,radius)
+    	mob_start_sel = startsel[0]
+    	ref_start_sel = startsel[1]
+    	if mob_start_sel.numAtoms() < 5:
+    		return startsel
 
-    	mob_start_sel = self.mobile.select("index "+arraystr(startids_mob))
-    	ref_start_sel = self.reference.select("index "+arraystr(startids_ref))
     	t = pd.calcTransformation(mob_start_sel,ref_start_sel,weights=mob_start_sel.getMasses())
+        pd.applyTransformation(t,self.mobile)
+
+        count = 0
+        prev = self.superimpose(eps)
+        curr = self.superimpose(eps)
+
+        while prev[0].numAtoms() > curr[0].numAtoms():
+            prev = curr
+            curr = self.superimpose(eps)
+            count += 1
+
+            if count == self.maxcounter:
+                print "convergence - warning: a domain did not converge."
+                break
+
+            if curr[0].numAtoms() < 5:
+                return [None, None]
+
+        return curr
+
+    def update_boundaries(self,eps,number):
+        mob_coord = self.mobFullCA.getCoords()
+        ref_coord = self.refFullCA.getCoords()
+
+        newsup_rmsd = []
+        newindex = []
+
+        i = 0
+
+        while i < len(mob_coord):
+            mcrd = mob_coord[i]
+            rcrd = ref_coord[i]
+            dist = pd.calcDistance(mcrd,rcrd)
+            if dist < self.sup_rmsd[i]:
+                newsup_rmsd.append(dist)
+                if dist < eps:
+                    newindex.append(number)
+                else:
+                    newindex.append(self.dindex[i])
+            else:
+                newsup_rmsd.append(self.sup_rmsd[i])
+                newindex.append(self.dindex[i])
+
+            i += 1
+
+        self.sup_rmsd = newsup_rmsd
+        self.dindex = newindex
+
+    def partition(self,eps):
+        # initialize
+        self.mobCA = self.mobFullCA
+        self.refCA = self.refFullCA
+        self.sup_rmsd = [999]*self.mobFullCA.numAtoms()
+        self.dindex = [-1]*self.mobFullCA.numAtoms()
+        self.domain_count = 0
+        seedradius = self.initrad
+
+        # partition the protein
+        while self.domain_count < self.ndomains:
+            domain = self.convergence(eps, seedradius)
+            mob_domain = domain[0]
+            ref_domain = domain[1]
+
+            if mob_domain.numAtoms() == 0:
+                print "partition> trying 20% smaller seed radius..."
+                seedradius *= 0.8
+                continue
+
+            # remove from ca1 and ca2 those atoms which have been found
+            mob_dom_list = mob_domain.getIndices()
+            ref_dom_list = ref_domain.getIndices()
+            mobCA_list = self.mobCA.getIndices()
+            refCA_list = self.refCA.getIndices()
+            mob_disjoint_l = [i for i in mobCA_list if not i in mob_dom_list]
+            ref_disjoint_l = [i for i in refCA_list if not i in ref_dom_list]
+
+
+            if len(mob_disjoint_l) > 0:
+                self.mobCA = self.indexSel(self.mobile, mob_disjoint_l)
+                self.refCA = self.indexSel(self.reference, ref_disjoint_l)
+
+            if mob_domain.numAtoms() > 4:
+                seedradius = self.initrad
+                self.update_boundaries(eps, self.domain_count)
+
+            self.domain_count += 1
+
+            if self.domain_count == 1: 
+                print "partition> now have 1 domain"
+            else:
+                print "partition> now have %d domains"%self.domain_count
+
+            if len(mob_disjoint_l) < self.cutdom:
+                print "partittion> protein now fully partitioned"
+                break
+
+            self.mobDomList = {}
+            self.refDomList = {}
+            currentDomains = set(self.dindex)        
+            for d in currentDomains:
+                self.mobDomList[d] = []
+                self.refDomList[d] = []
+
+            mob_ids = self.mobFullCA.getIndices()
+            ref_ids = self.refFullCA.getIndices()
+
+            i = 0
+            for d in self.dindex:
+                self.mobDomList[d].append(mob_ids[i])
+                self.refDomList[d].append(ref_ids[i])
+                i += 1
+
+            for d in self.mobDomList.keys():
+                print self.indexSel(self.mobile, self.mobDomList[d]).getResnums()
+
 
     def hinge (beforeM, afterM, beforeR, afterR, mobile, rotmat): 
         '''
@@ -198,11 +327,13 @@ def main():
 
     hf = HingeFind(mobile,reference)
     
-    # # test criterion
+    # test criterion
     # selstr = "resid 92 to 100 104 to 140 143 to 218 220 to 250 and name CA"
     # moveBy = pd.calcTransformation(mobile.select(selstr),reference.select(selstr))
     # pd.applyTransformation(moveBy,mobile)
-    # print hf.criterion(selstr,selstr,1.0)
+    # print hf.criterion(mobile.select(selstr),reference.select(selstr),1.0)
+
+    hf.partition(1.0)
 
     # D1str = args.domain1
     # D1_0 = structure1.select(D1str)
